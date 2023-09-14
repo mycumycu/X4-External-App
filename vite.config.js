f809ec3ca1f717ab207e6b5b3926d9f74a73133b
Wim Looman <git@nemo157.com>
2023-09-14 20:30:40 +0200

    Add PWA manifest generation to allow installing as fullscreen app

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,36 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        VitePWA({
+            manifest: {
+                name: 'X4 External App',
+                short_name: 'X4 External App',
+                description: 'External app for X4 Foundations game',
+                theme_color: '#2D3035',
+                background_color: '#2D3035',
+                icons: [
+                     {
+                         src: '/favicon.ico',
+                         sizes: '32x32',
+                         type: 'image/ico',
+                     },
+                     {
+                         src: '/x4-logo-192.png',
+                         sizes: '192x192',
+                         type: 'image/png',
+                     },
+                ]
+            },
+            devOptions: {
+                enabled: true,
+            },
+        }),
+    ],
     server: {
         host: true,
         proxy: {
