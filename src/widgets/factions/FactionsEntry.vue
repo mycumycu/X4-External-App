2493684ba2cfc41154f63ee1093103b63673a81e
Mycu <tomek@mycek.pl>
2024-01-11 08:41:53 +0100

    Create factions widget

---
+++ src/widgets/factions/FactionsEntry.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="col-auto px-lg-0 ps-0 pe-2 d-flex justify-content-center" :style="{color: rgbToHex(entry.color)}">
+    <div class="lh-1 px-0 py-1 text-center faction-block" :class="this.exposeClass">
+      <span class="d-inline-block licences military" :class="{filled: settings.showLicenceIndicators && entry.hasMilitaryLicence}" title="Military licence"></span>
+      <span class="d-inline-block licences capital" :class="{filled: settings.showLicenceIndicators && entry.hasCapitalLicence}" title="Capital ship licence"></span>
+      <div class="position-relative">{{ entry.shortname }}<sup v-if="settings.showRelationChanges" class="text-muted position-absolute relation-change" title="Recent relation change">{{ getRelationChange() }}</sup></div>
+      <span class="relation">&nbsp;&nbsp;{{ getDisplayValue(entry.relation) }}</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'settings',
+    'entry',
+  ],
+  methods: {
+    /**
+     * @return {string}
+     */
+    componentToHex(c) {
+      let hex = c.toString(16);
+      return hex.length === 1 ? "0" + hex : hex;
+    },
+
+    /**
+     * @return {string}
+     */
+    rgbToHex(color) {
+      if (color === undefined) {
+        return "#fff";
+      }
+      return "#" + this.componentToHex(color.r) + this.componentToHex(color.g) + this.componentToHex(color.b);
+    },
+    /**
+     * @return {string}
+     */
+    getDisplayValue(value) {
+      if (value > 0) {
+        return `+${value}`;
+      }
+      return value;
+    },
+    /**
+     * @return {string}
+     */
+    getRelationChange() {
+      if (this.relationChange === 0) {
+        return '';
+      }
+      return this.getDisplayValue(this.relationChange);
+    },
+  },
+  watch: {
+    entry: {
+      /**
+       * @param newData Information about the user.
+       * @param newData.relation Relation to the faction
+       * @param newData.name Faction full name
+       * @param newData.shortname Faction shortname
+       * @param newData.hasMilitaryLicence If the player has a military licence for the faction
+       * @param newData.hasCapitalLicence If the player has a capital licence for the faction
+       * @param newData.currentGameTime Current game time
+       * @param {object} oldData
+       */
+      handler(newData, oldData) {
+        let timeDifference = newData.currentGameTime - oldData.currentGameTime;
+        if (newData.relation !== oldData.relation) {
+          this.relationChange = newData.relation - oldData.relation;
+          this.exposeClass = 'expose'
+
+          if (this.previousFaction !== newData.shortname || timeDifference < 0 || timeDifference > 30) {
+            /*
+              in case of:
+                * faction change (e.g. when previous faction was hidden in the options)
+                * loading a savegame (so the time differs)
+              reset the relation change
+             */
+
+            this.relationChange = 0;
+            this.exposeClass = null;
+          }
+
+          setTimeout(() => {
+            this.exposeClass = null;
+          }, 2000);
+
+        }
+
+        this.previousFaction = newData.shortname;
+      },
+    },
+  },
+  computed: {},
+  data() {
+    return {
+      relationChange: 0,
+      previousFaction: null,
+      exposeClass: null,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./scss/widget.scss";
+</style>
---
+++ src/widgets/factions/FactionsWidget.vue
@@ -0,0 +1,164 @@
+<template>
+  <widget>
+    <template #header>
+      <div class="d-flex justify-content-between">
+        <h4 class="card-title pb-0 mb-0">Factions</h4>
+        <div>
+          <font-awesome-icon class="cursor-pointer" :icon="`cogs`" data-bs-toggle="modal" data-bs-target="#factions-settings"/>
+          <Modal id="factions-settings" title="Factions settings" size="modal-lg">
+            <FactionsSettings :settings="factions.settings"/>
+          </Modal>
+        </div>
+      </div>
+    </template>
+
+    <perfect-scrollbar class="factions resizable-element" data-min-resizable-height="90">
+      <div class="row mx-0">
+        <template v-for="(entry, index) in factions.list">
+          <factions-entry
+              :settings="factions.settings"
+              :entry="entry"/>
+        </template>
+      </div>
+    </perfect-scrollbar>
+  </widget>
+</template>
+
+<script>
+
+import Widget from "../Widget.vue";
+import Modal from "../../components/Modal.vue";
+import FactionsSettings from "./FactionsSettings.vue";
+import SearchBar from "../../components/SearchBar.vue";
+import {reactive} from "vue";
+import LogbookEntry from "../logbook/LogbookEntry.vue";
+import FactionsEntry from "./FactionsEntry.vue";
+
+export default {
+  components: {
+    FactionsEntry,
+    LogbookEntry,
+    Modal, FactionsSettings: FactionsSettings, SearchBar, Widget
+  },
+  props: {
+    gameData: Object,
+    maxHeight: {
+      type: Number,
+      default: 100
+    },
+  },
+  /**
+   */
+  watch: {
+    gameData: {
+      handler(newData, oldData) {
+        this.parseFactionsData(newData)
+      },
+    },
+    'factions.settings': {
+      handler(newList, oldList) {
+        //
+      },
+      deep: true
+    },
+  },
+  methods: {
+    /**
+     *
+     */
+    parseFactionsData(gameData) {
+      let excludedFactions = this.factions.settings.excludedFactions.split(',').map((faction) => {
+        return faction.trim().toLowerCase();
+      });
+
+      // filter player and excluded factions and those with relations disabled
+      this.factions.list = gameData.filter((faction) => {
+        return faction.id !== 'player' &&
+            !excludedFactions.includes(faction.shortname.toLowerCase()) &&
+            this.getRelationLevels().includes(faction.relation);
+      });
+    },
+    /**
+     * Get allowed relation levels array
+     */
+    getRelationLevels() {
+      return this.factions.settings.relationLevels.map((level) => {
+        return level.enabled ? this.arrayFill(level.min, level.max) : [];
+      }).flat();
+    },
+    /**
+     * Fills an array with a sequence of numbers.
+     *
+     * @param {number} start - The starting value of the sequence.
+     * @param {number} stop - The ending value of the sequence.
+     *
+     * @return {Array} - The filled array containing the sequence of numbers.
+     */
+    arrayFill(start, stop) {
+      return Array.from(
+          { length: (stop - start) + 1 },
+          (value, index) => start + index
+      );
+    }
+  },
+  computed: {
+    //
+  },
+  mounted() {
+    //
+  },
+  /**
+   * @return {{factions: {list: [], settings: {excludedFactions: string, showRelationChanges: boolean, relationLevels: {index: number, name: string, min: number, max: number, enabled: boolean}[]}}}}
+   */
+  data() {
+    return {
+      factions: {
+        list: [],
+        settings: reactive(
+            JSON.parse(localStorage.getItem("factionsSettings")) || {
+              excludedFactions: '',
+              showRelationChanges: true,
+              showLicenceIndicators: true,
+              relationLevels: [
+                {
+                  name: 'ally',
+                  min: 20,
+                  max: 30,
+                  enabled: true
+                },
+                {
+                  name: 'enemy',
+                  min: -19,
+                  max: -10,
+                  enabled: true
+                },
+                {
+                  name: 'friend',
+                  min: 10,
+                  max: 19,
+                  enabled: true
+                },
+                {
+                  name: 'hostile',
+                  min: -30,
+                  max: -20,
+                  enabled: true
+                },
+                {
+                  name: 'neutral',
+                  min: -9,
+                  max: 9,
+                  enabled: true
+                },
+              ],
+            }
+        ),
+      },
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./scss/widget.scss";
+</style>
